bbf4afb0047e5672f067b03a52f9ff4cc0e09fb1
Carlo <hola.carlodominguez@gmail.com>
2025-01-20 23:41:59 +0100

    repro

---
+++ app/routes/index.tsx
@@ -0,0 +1,53 @@
+import { createFileRoute } from "@tanstack/react-router"
+import { createServerFn } from "@tanstack/start"
+import { parseWithZod } from "@conform-to/zod"
+import { z } from "zod"
+import { useForm } from "@conform-to/react"
+import { get } from "http"
+
+const schema = z.object({
+  username: z.string(),
+})
+
+const getCount = createServerFn({ method: "POST" })
+  .validator((data: FormData) => data)
+  .handler(async (options) => {
+    const submission = parseWithZod(options.data, { schema })
+    return submission.reply() // Returning this causes the error
+  })
+
+export const Route = createFileRoute("/")({
+  component: Home,
+})
+
+function Home() {
+  const [form, fields] = useForm({
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema })
+    },
+    async onSubmit(event, context) {
+      event.preventDefault()
+      await getCount({
+        data: context.formData,
+      })
+    },
+  })
+
+  return (
+    <form
+      id={form.id}
+      action={getCount.url}
+      onSubmit={form.onSubmit}
+      method="post"
+      encType="multipart/form-data"
+    >
+      <div>{form.errors}</div>
+      <div>
+        <label>Username</label>
+        <input type="text" name={fields.username.name} />
+        <div>{fields.username.errors}</div>
+      </div>
+      <button>Submit</button>
+    </form>
+  )
+}
